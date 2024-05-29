2f03a4368d394acc1404a06737501d26b6f953e0
shpre1236 <shpre1236@kaist.ac.kr>
2024-05-29 18:16:40 +0900

    03_ticket_v1 > 12_outro finished

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,62 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        if !Self::validate_product_name(&product_name) || !Self::validate_quantity(quantity) || !Self::validate_unit_price(unit_price) {
+            panic!();
+        }
+
+        Self {
+            product_name,
+            quantity,
+            unit_price
+        }
+    }
+    fn validate_product_name(product_name: &String) -> bool {
+        !product_name.is_empty() && product_name.len() < 300
+    }
+    fn validate_quantity(quantity: u32) -> bool {
+        quantity > 0
+    }
+    fn validate_unit_price(unit_price: u32) -> bool {
+        unit_price > 0
+    }
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+    pub fn set_product_name(&mut self, product_name: String) {
+        if !Self::validate_product_name(&product_name) {
+            panic!();
+        }
+        self.product_name = product_name;
+    }
+    pub fn set_quantity(&mut self, quantity: u32) {
+        if !Self::validate_quantity(quantity) {
+            panic!();
+        }
+        self.quantity = quantity;
+    }
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+        if !Self::validate_unit_price(unit_price) {
+            panic!();
+        }
+        self.unit_price = unit_price;
+    }
+}
